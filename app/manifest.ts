ab05492256003682dab237d5865bdb2b576d8b29
snuspage-ux <snuspage@gmail.com>
2026-03-09 18:31:16 +0100

    fix: manifest icon purpose type

--- app/manifest.ts
+++ app/manifest.ts
@@ -20,7 +20,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
